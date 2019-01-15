e9324148b7a91686ab3cc695eec61a1db2654da4
Lailson Henrique <lailson.pi@gmail.com>
2019-01-14 22:17:18 -0300

    08: transformar farenheit em celsius

---
+++ 01_EstruturaSequencial/08_calcula_salario.py
@@ -0,0 +1,4 @@
+valor = float(input('Quanto você ganha por hora trabalhada ?'))
+horas = float(input('Quantas horas você trabalhou ?'))
+salario = valor * horas
+print('O salário desse mês é R$',salario)
---
+++ 01_EstruturaSequencial/09_ Farenheit_para_celsius.py
@@ -0,0 +1,3 @@
+farenheit = float(input('Digite a temperatura em Farenheit: '))
+celsius = (5 * (farenheit - 32) / 9)
+print(farenheit,'F equivale a',celsius,'C')
