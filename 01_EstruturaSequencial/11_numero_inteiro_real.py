2eaac5608600d81d2ae0232ed3ba214bef0fa8ef
Lailson Henrique <lailson.pi@gmail.com>
2019-01-14 23:04:27 -0300

    11: dois inteiros e um float

---
+++ 01_EstruturaSequencial/11_numero_inteiro_real.py
@@ -0,0 +1,4 @@
+inteiro1 = int(input('Digite um número inteiro: '))
+inteiro2 = int(input('Digite outro número inteiro: '))
+real = float(input('Digite um número real: '))
+print ("O produto do dobro do primeiro com metade do segundo é ", (inteiro1*2) * (inteiro2/2.0) )
